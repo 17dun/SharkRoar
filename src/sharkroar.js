b4732d836ae5ff28808e20ba730a294a0a67f944
island205 <island205@gmail.com>
2013-04-18 11:34:41 +0800

    compass

--- src/sharkroar.js
+++ src/sharkroar.js
@@ -1,13 +1,14 @@
 (function (WIN) {
-    var service = 'http://192.168.7.92:3000/pusherror/'
+    // var service = 'http://192.168.7.92:3000/pusherror/'
+    var service = 'http://cat.qa.dianpingoa.com/cat/r/jsError'
     WIN.onerror = function (errorMsg, fileUrl, lineNumber) {
         var e = encodeURIComponent,
         timestamp = Date.now()
-        (new Image()).src = service +
-            '?e=' + e(errorMsg) +
-            '&f=' + e(fileUrl) +
-            '&l=' + e(lineNumber) +
-            '&t=' + timestamp
+        ;(new Image()).src = service +
+            '?error=' + e(errorMsg) +
+            '&file=' + e(fileUrl) +
+            '&line=' + e(lineNumber) +
+            '&timestamp=' + timestamp
     }
 })(window)
 
