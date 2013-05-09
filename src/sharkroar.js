faaaf2c74e5726d3ed631f41b2d967e96dc9ad45
island205 <island205@gmail.com>
2013-05-09 18:26:28 +0800

    support statistical percentage

--- src/sharkroar.js
+++ src/sharkroar.js
@@ -1,14 +1,18 @@
-(function (WIN) {
-    var service
-    service = 'http://example.com/jserror.gif'
-    WIN.onerror = function (errorMsg, fileUrl, lineNumber) {
-        var e = encodeURIComponent,
-        timestamp = Date.now();
-        (new Image()).src = service +
-            '?error=' + e(errorMsg) +
-            '&file=' + e(fileUrl) +
-            '&line=' + e(lineNumber) +
-            '&timestamp=' + timestamp
+(function () {
+    var config = {
+        service: 'http://example.com/jserror.gif',
+        percent: 0.1   
     }
-})(window)
+    if (Math.random() < config.percent) {
+        window.onerror = function (errorMsg, fileUrl, lineNumber) {
+            var e = encodeURIComponent,
+            timestamp = Date.now();
+            (new Image()).src = service +
+                '?error=' + e(errorMsg) +
+                '&file=' + e(fileUrl) +
+                '&line=' + e(lineNumber) +
+                '&timestamp=' + timestamp
+        }
+    }
+})()
 
